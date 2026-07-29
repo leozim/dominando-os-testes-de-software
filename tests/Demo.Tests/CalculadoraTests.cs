36f0884d59ddc025d4981f1b53b32da8bdb5b423
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-29 15:30:21 -0300

    teste com Theory e InlineData

--- tests/Demo.Tests/CalculadoraTests.cs
+++ tests/Demo.Tests/CalculadoraTests.cs
@@ -14,4 +14,20 @@ public class CalculadoraTests
         // Assert
         Assert.Equal(3, resultado);
     }
+
+    [Theory]
+    [InlineData(1, 3, 4)]
+    [InlineData(2, 3, 5)]
+    [InlineData(3, 3, 6)]
+    public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+        
+        // Act
+        var resultado = calculadora.Somar(v1, v2);
+
+        // Assert
+        Assert.Equal(total, resultado);
+    }
 }
