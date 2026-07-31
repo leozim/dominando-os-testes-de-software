62d67bcba8cd94487f3fb0befbca553abb0c3a92
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-31 01:30:57 -0300

    assert object types

--- tests/Demo.Tests/AssertingObjectTypestests.cs
+++ tests/Demo.Tests/AssertingObjectTypestests.cs
@@ -2,5 +2,23 @@
 
 public class AssertingObjectTypestests
 {
-    
+    [Fact]
+    public void FuncionarioFactory_Criar_DeveRetornarTipoFuncionario()
+    {
+        // Arrange & Act
+        var funcionario = FuncionarioFactory.Criar("Leonardo", 10000);
+
+        // Assert
+        Assert.IsType<Funcionario>(funcionario);
+    }
+
+    [Fact]
+    public void FuncionarioFactory_Criar_DeveRetornarTipoDerivadoPessoa()
+    {
+        // Arrange & Act
+        var funcionario = FuncionarioFactory.Criar("Leonardo", 10000);
+        
+        // Assert
+        Assert.IsAssignableFrom<Pessoa>(funcionario);
+    }
 }
