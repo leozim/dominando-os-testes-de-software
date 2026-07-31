493d5121dedb80cc67e73d06b730323674296bd8
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-31 00:31:43 -0300

    preparando cenario par Assers

---
+++ src/Demo.DominantoTestesDeUnidades/StringsTools.cs
@@ -0,0 +1,10 @@
+﻿namespace DominandoTestesDeUnidades
+{
+    public class StringsTools
+    {
+        public string Unir(string nome, string sobrenome)
+        {
+            return $"{nome} {sobrenome}";
+        }
+    }
+}
