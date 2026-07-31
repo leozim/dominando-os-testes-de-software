7384d244895b1f32ac69f43d90f98772b052ff8c
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-07-31 00:50:03 -0300

    assert para string estudado

---
+++ tests/Demo.Tests/AssertStrings.cs
@@ -0,0 +1,71 @@
+﻿namespace DominandoTestesDeUnidades.Tests;
+
+public class AssertStrings
+{
+    [Fact]
+    public void StringsTools_UnirMoes_RetornarNomeCompleto()
+    {
+        // Arrange
+        var sut = new StringsTools();
+        
+        // Act
+        var nomeCompleto = sut.Unir("Leonardo", "Mariz Bezerra");
+        
+        // Assert
+        Assert.Equal("Leonardo Mariz Bezerra", nomeCompleto, true);
+    }
+
+    [Fact]
+    public void StringsTools_UnirNomes_DeveConterTrecho()
+    {
+        // Arrange
+        var sut = new StringsTools();
+        
+        // Act
+        var nomCompleto = sut.Unir("Leonardo", "Mariz Bezerra");
+        
+        // Assert
+        Assert.Contains("nardo", nomCompleto);
+    }
+
+    [Fact]
+    public void StringsTools_UnirNome_DeveComecarCom()
+    {
+        // Arrange
+        var sut = new StringsTools();
+        
+        // Act
+        var nomeCompleto = sut.Unir("Leonardo", "Martin");
+        
+        // Assert
+        Assert.StartsWith("Leon", nomeCompleto);
+    }
+
+    [Fact]
+    public void StringsTools_UnirNomes_DeveAcabarCom()
+    {
+        // Arramge
+        var sut = new StringsTools();
+        
+        // Act
+        var nomeCompleto = sut.Unir("Leonardo", "Mariz Bezerra");
+        
+        //Assert
+        Assert.EndsWith("erra", nomeCompleto);
+        
+    }
+
+    [Fact]
+
+    public void StringsTools_UnirNomes_ValidarExpressaoRegular()
+    {
+        // Arrange
+        var sut = new StringsTools();
+        
+        // Act
+        var nomeCompleto = sut.Unir("Leonardo", "Mariz Bezerra");
+        
+        // Assert
+        Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", nomeCompleto);
+    }
+}
