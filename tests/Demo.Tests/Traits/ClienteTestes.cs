0ab7bd1a825f0d5bb9630538abfb0b7b5365a248
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-03 23:03:46 -0300

    primeiro teste com novo cliente

---
+++ tests/Demo.Tests/Traits/ClienteTestes.cs
@@ -0,0 +1,28 @@
+﻿using Features.Clientes;
+
+namespace DominandoTestesDeUnidades.Tests.Traits;
+
+public class ClienteTestes
+{
+    [Fact]
+    public void Cliente_NovoCliente_DeveEstarValido()
+    {
+        // Arrange
+        var cliente = new Cliente(
+            Guid.NewGuid(),
+            "Leoardo",
+            "Mariz",
+            DateTime.Now.AddYears(-31),
+            "leonardo@hotmail.com",
+            true,
+            DateTime.Now);
+        
+        // Act
+        var result = cliente.EhValido();
+
+        // Assert
+        Assert.True(result);
+        Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+        Assert.Empty(cliente.ValidationResult.Errors);
+    }
+}
