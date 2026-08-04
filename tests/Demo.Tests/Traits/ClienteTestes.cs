ddf82ae319b46644d3b6dec6bd47d4e2724776c1
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-03 23:22:19 -0300

    categorizando testes com Traits

--- tests/Demo.Tests/Traits/ClienteTestes.cs
+++ tests/Demo.Tests/Traits/ClienteTestes.cs
@@ -4,7 +4,8 @@ namespace DominandoTestesDeUnidades.Tests.Traits;
 
 public class ClienteTestes
 {
-    [Fact]
+    [Fact(DisplayName = "Novo Clientre Valido")]
+    [Trait("Categoria", "Cliente Trait Testes")]
     public void Cliente_NovoCliente_DeveEstarValido()
     {
         // Arrange
@@ -24,5 +25,27 @@ public class ClienteTestes
         Assert.True(result);
         Assert.Equal(0, cliente.ValidationResult.Errors.Count);
         Assert.Empty(cliente.ValidationResult.Errors);
+    }
+
+    [Fact(DisplayName = "Novo Cliente Inválido")]
+    [Trait("Categoria", "Cliente Trait Testes")]
+    public void Cliente_NovoCliente_DeveEstarInvalido()
+    {
+        // Arrange
+        var cliente = new Cliente(
+            Guid.NewGuid(),
+            "",
+            "",
+            DateTime.Now.AddYears(-11),
+            "leonardo#hotmail.com",
+            true,
+            DateTime.Now);
+        // Act
+        var result = cliente.EhValido();
+        
+        // Assert
+        Assert.False(result);
+        Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+        Assert.NotEmpty(cliente.ValidationResult.Errors);
     }
 }
