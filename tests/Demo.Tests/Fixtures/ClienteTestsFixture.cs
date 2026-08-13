48114bcb7f6453f5ae814ca6509cf4517f7806ff
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-12 22:59:57 -0300

    usando bogus framework para gerar dados humanos

--- tests/Demo.Tests/Fixtures/ClienteTestsFixture.cs
+++ tests/Demo.Tests/Fixtures/ClienteTestsFixture.cs
@@ -1,17 +1,42 @@
-﻿using Features.Clientes;
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
 
 namespace DominandoTestesDeUnidades.Tests.Fixtures;
 
 public class ClienteTestsFixture : IDisposable
 {
-    public Cliente GerarClienteValido() =>
-        new Cliente(Guid.NewGuid(),
+    public Cliente GerarClienteValido()
+    {
+        var genero = new Faker().PickRandom<Name.Gender>();
+        /*var email = new Faker().Internet.Email("Leonardo", "Mariz", "gmail");
+        var clienteFaker = new Faker<Cliente>();
+        clienteFaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());*/
+
+        var cliente2 = new Faker<Cliente>("pt_BR")
+            .CustomInstantiator(f => new Cliente(
+                Guid.NewGuid(),
+                f.Name.FirstName(genero),
+                f.Name.LastName(genero),
+                f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                "",
+                true,
+                DateTime.Now
+                ))
+            .RuleFor(c => c.Email, (f,c) => 
+                    f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower())
+                    );
+        
+        var cliente = new Cliente(Guid.NewGuid(),
             "Leonardo",
             "Mariz",
             DateTime.Now.AddYears(-31),
             "leonardo@hotmail.com",
             true,
             DateTime.Now);
+
+        return cliente2;
+    }
 
     public Cliente GerarClienteInvalido() => new Cliente(
         Guid.NewGuid(),
