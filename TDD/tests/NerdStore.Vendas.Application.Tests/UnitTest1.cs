6fa12c8e84354a4714abbb28a2782a8b67debab0
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-16 01:31:43 -0300

    preparado projeto de testes para tdd

---
+++ TDD/tests/NerdStore.Vendas.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace NerdStore.Vendas.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ TDD/tests/NerdStore.Vendas.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace NerdStore.Vendas.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
