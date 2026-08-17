2129f145a310c5126bb725e3c4e1d9e584b6b56b
Leonardo Mariz Bezerra <leonardomariz@alu.ufc.br>
2026-08-17 00:09:29 -0300

    escrevendo primeiro teste com TDD

---
+++ TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NerdStore.Vendas.Domain;
+
+public class Pedido
+{
+    private readonly List<PedidoItem> _pedidoItems;
+    
+    public decimal ValorTotal { get; private set; }
+    public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+    
+    public Pedido()
+    {
+        _pedidoItems = new List<PedidoItem>();
+    }
+    public void AdicionarItem(PedidoItem pedidoItem)
+    {
+        _pedidoItems.Add(pedidoItem);
+        ValorTotal = PedidoItems.Sum(i => i.Quantidade * i.ValorUnitario);
+    }
+}
---
+++ TDD/tests/NerdStore.Vendas.Domain.Tests/PedidoTests.cs
@@ -0,0 +1,20 @@
+﻿namespace NerdStore.Vendas.Domain.Tests;
+
+public class PedidoTests
+{
+    [Fact(DisplayName = "Adicionar Item Pedido Vazio")]
+    [Trait("Categoria", "Pedido Tests")]
+    public void AdicionarItempedido_NovoPedido_DeveAtualizarValor()
+    {
+        // Arrange
+        var pedido = new Pedido();
+        var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+
+        // Act
+        pedido.AdicionarItem(pedidoItem);
+        
+        // Assert
+        Assert.Equal(200, pedido.ValorTotal);
+    }
+    
+}
